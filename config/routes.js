383a49544a57b4e255a727d8b078a619b3dd7d91
Daan <daanraatjes@gmail.com>
2017-03-03 21:14:48 +0100

    Fixed indentation

--- config/routes.js
+++ config/routes.js
@@ -1,36 +1,34 @@
 module.exports = function(app, passport) {
-	
-	
-	app.get('/', function(req, res) {
-	//	connection.query('SELECT * FROM anouncement',function(err,rows){
-			//if(err) throw err;
-			
-			res.render('file', { message: req.flash('failureFlash') });
-	//	});
-	});
-	
-	app.get('/main', isLoggedIn, function(req, res) {
-        res.render('loggedIn.ejs', {
-            user : req.user 
+
+
+    app.get('/', function(req, res) {
+        res.render('file', {
+            message: req.flash('failureFlash')
         });
     });
-	
-	
-	app.get('/logout', function(req, res) {
+
+    app.get('/main', isLoggedIn, function(req, res) {
+        res.render('loggedIn.ejs', {
+            user: req.user
+        });
+    });
+
+
+    app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     });
-    
+
     app.post('/', passport.authenticate('login', {
-        successRedirect : '/main',
-        failureRedirect : '/', 
-        failureFlash : true 
+        successRedirect: '/main',
+        failureRedirect: '/',
+        failureFlash: true
     }));
-     
+
     // sends 404 for random urls
-    app.use(function(req, res){
-       res.render('notFound.ejs');
-	});
+    app.use(function(req, res) {
+        res.render('notFound.ejs');
+    });
 
 };
 
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var http = require('http');  
+var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var express = require('express');
 var passport = require('passport');
 var app = express();
-var flash    = require('connect-flash');
-var morgan       = require('morgan');
+var flash = require('connect-flash');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 
 require('./config/passport')(passport);
@@ -17,8 +17,11 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser());
-app.use(session({ secret: 'summerwinter', saveUninitialized: true,
-  resave: true, }));
+app.use(session({
+    secret: 'summerwinter',
+    saveUninitialized: true,
+    resave: true,
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
