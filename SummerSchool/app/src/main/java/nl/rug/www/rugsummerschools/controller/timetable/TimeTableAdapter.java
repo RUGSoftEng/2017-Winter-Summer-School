9d5547ccce7b82ce2d9771d552f1c1fa199a4921
lasking88 <j.k.oh@student.rug.nl>
2018-01-17 23:18:41 +0100

    New design of time table as like google calendar agenda view

---
+++ SummerSchool/app/src/main/java/nl/rug/www/rugsummerschools/controller/timetable/TimeTableAdapter.java
@@ -0,0 +1,39 @@
+package nl.rug.www.rugsummerschools.controller.timetable;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import nl.rug.www.rugsummerschools.model.EventsPerDay;
+
+
+public class TimeTableAdapter extends RecyclerView.Adapter<TimeTableHolder> {
+
+    private Context mContext;
+    private List<EventsPerDay> mEventsPerDayList;
+
+    public TimeTableAdapter(List<EventsPerDay> eventsPerDays, Context context) {
+        mContext = context;
+        mEventsPerDayList = eventsPerDays;
+    }
+
+    @Override
+    public TimeTableHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        return new TimeTableHolder(layoutInflater, parent, mContext);
+    }
+
+    @Override
+    public void onBindViewHolder(TimeTableHolder holder, int position) {
+        EventsPerDay eventsPerDay = mEventsPerDayList.get(position);
+        holder.bind(eventsPerDay);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mEventsPerDayList.size();
+    }
+}
